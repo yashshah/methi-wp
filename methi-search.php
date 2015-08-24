9b562799577863743635629d5f08097c9277434f
Siddharth Kothari <sids.aquarius@gmail.com>
2015-08-23 18:47:34 -0700

    update search plugin metadata

--- methi-search.php
+++ methi-search.php
@@ -1,5 +1,10 @@
 <?php
 /*
 Plugin Name: Methi Search
+Version: 0.1
+Plugin URI: https://github.com/siddharthlatest/methi-wp
+Description: Methi is an awesome realtime, mobileview optimized search for your wordpress blog, powered by appbase.io.
+Author: Siddharth Kothari
+Author URI: https://twitter.com/siddharthlatest
 */
 ?>
