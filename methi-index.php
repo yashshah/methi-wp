9f22093ec2ab82e72af1c454fafe001859f959d0
ilyasballuwala <ilyasballuwala8@gmail.com>
2015-08-30 17:54:22 +0530

    get only post data instead of all post parameter

--- methi-index.php
+++ methi-index.php
@@ -1,10 +1,36 @@
 <?php
   function add_wpdata_to_appbase() {
     global $wpdb;
+	global $post;
     $APPBASE_APPNAME = "faltu";
     $APPBASE_USERNAME = "dAIbIAm5W";
     $APPBASE_PASSWORD = "d91257af-1fe4-44ab-84cb-e93cb3768b92";
-    $posts = $wpdb->get_results("SELECT * from $wpdb->posts");
+  
+	 $args = array(
+		'posts_per_page'   => -1,
+		'offset'           => 0,
+		'category'         => '',
+		'category_name'    => '',
+		'orderby'          => 'date',
+		'order'            => 'DESC',
+		'include'          => '',
+		'exclude'          => '',
+		'meta_key'         => '',
+		'meta_value'       => '',
+		'post_type'        => 'post',
+		'post_mime_type'   => '',
+		'post_parent'      => '',
+		'author'	   => '',
+		'post_status'      => 'publish',
+		'suppress_filters' => true 
+	 );
+
+	$posts = get_posts($args);
+	
+	/*echo "<pre>";
+	print_r($posts);
+	die;*/
+	
     foreach($posts as $post) {
       $args = array(
         'headers' => array(
@@ -12,9 +38,11 @@
         ),
         'body' => json_encode(array(
           "title" => $post->post_title,
-          "created_at" => $post->post_date,
+		  "body" => $post->post_content,
+		  "link" => get_permalink($post->ID),
+          "date" => $post->post_date,
           "author" => $post->post_author,
-          "body" => $post->post_content
+          
         ))
       );
       $response = wp_remote_post("https://".$APPBASE_USERNAME.":".$APPBASE_PASSWORD."@scalr.api.appbase.io/".$APPBASE_APPNAME."/article/", $args);
--- methi-search.php
+++ methi-search.php
@@ -2,11 +2,16 @@
 /*
 Plugin Name: Methi Search
 Version: 0.1
-Plugin URI: https://github.com/siddharthlatest/methi-wp
+Plugin URI: https://github.com/yashshah/methi-wp
 Description: Methi is an awesome realtime, mobileview optimized search for your wordpress blog, powered by appbase.io.
 Author: Siddharth Kothari
-Author URI: https://twitter.com/siddharthlatest
+Author URI: https://twitter.com/yashshah
 */
+
+// For debugging purposes
+//error_reporting(E_ALL);
+//ini_set("display_errors", 1); 
+//define('WP-DEBUG', true);
 
 if (!defined('ABSPATH')) {
 	die('script kiddies, f*ck off');
