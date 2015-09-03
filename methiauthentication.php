ba5da4d102ea0e1df5cf6d6058e1a5e065eaf725
Yash Shah <mail@yashshah.com>
2015-09-03 15:39:20 -0700

    Add Authentication module

---
+++ methiauthentication.php
@@ -0,0 +1,16 @@
+<h2>Methi Secret Token For Post Indexing</h2>
+<form name="autentication_form" action="" method="post">
+<table id="methiauth"> 
+        <tr>
+            <td>Appbase Secret Token : </td>
+			<td><input type='text' name='appbase_secret_token' class="appbaseform-elements" id="appbase_secret_token" /></td>
+        </tr>
+		 <tr>
+        <td colspan="3"><input type="submit" name="submit_methi_authentication" value="SUBMIT" id="submit_methi_authentication"/></td>
+    </tr>
+</table>
+</form>
+
+<style type="text/css">
+.appbaseform-elements { width:400px; }
+</style>
