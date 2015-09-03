0e81aca774361ec36c05e20e558ec9732aea075e
Yash Shah <mail@yashshah.com>
2015-09-03 15:39:36 -0700

    Add uninstall module

---
+++ uninstall.php
@@ -0,0 +1,12 @@
+<?php
+
+// If uninstall is not called from WordPress, exit
+if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    exit();
+}
+ 
+$option_name = 'methi-secretkey-array';
+ 
+delete_option( $option_name );
+
+?>
