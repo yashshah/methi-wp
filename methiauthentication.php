6a7bb0a4a4aa169e390fa4c9f824a91259f67ace
Yash Shah <mail@yashshah.com>
2015-09-09 10:24:20 -0700

    Add progress bar while indexing posts

--- methiauthentication.php
+++ methiauthentication.php
@@ -1,4 +1,6 @@
+<?php global $pluginUrl; ?>
 <h2>Methi Secret Token For Post Indexing</h2>
+
 <form name="autentication_form" action="" method="post">
 <table id="methiauth"> 
         <tr>
@@ -6,11 +8,23 @@
 			<td><input type='text' name='appbase_secret_token' class="appbaseform-elements" id="appbase_secret_token" /></td>
         </tr>
 		 <tr>
-        <td colspan="3"><input type="submit" name="submit_methi_authentication" value="SUBMIT" id="submit_methi_authentication"/></td>
+        <td colspan="3"><input type="button" name="submitallpost" value="Index Post" id="submitallpost"/></td>
     </tr>
 </table>
 </form>
+<?php $count_posts = wp_count_posts()->publish; ?>
+<div id="import_record_process">
+<h2>Importing Records......</h2>
+<div class="percentagebar"><div class="actualprocess"></div></div>
+<div class="progress_container"><div class="percentage_container"><span id="precentageprogress">0</span>%</div> <div class="totalpostcount"><span id="postcountprogress">0</span>/<?php echo $count_posts; ?><div></div>
+</div>
 
 <style type="text/css">
 .appbaseform-elements { width:400px; }
+#import_record_process { background :#DDD; padding:5px 15px 20px; width:400px; margin:20px 0; display:none; }
+.percentagebar { display:inline-block; width:100%; height:15px; background:#ccc; position:relative; margin:5px 0 20px 0;}
+.actualprocess { display:inline-block; width:0%; height:15px; background:#019183; transition: all 0.5s ease 0s; -webkit-transition: all 0.5s ease 0s; -o-transition: all 0.5s ease 0s; -ms-transition: all 0.5s ease 0s; -moz-transition: all 0.5s ease 0s;}
+.progress_container { display:block; width:100%; font-size:17px; }
+.percentage_container { font-size:17px; font-weight:bold; display:inline; }
+.totalpostcount {text-align :right; float:right; display:block;}
 </style>
