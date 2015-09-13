ca37ea082bc84dfe50e47273eac3e4c948101c20
Yash Shah <mail@yashshah.com>
2015-09-13 12:28:17 -0700

    Refactor code in Methi class

--- methi-class-search.php
+++ methi-class-search.php
@@ -1,57 +1,84 @@
 <?php
+/*
+Plugin Name: Methi Search
+Version: 0.1
+Plugin URI: https://github.com/yashshah/methi-wp
+Description: Methi is an awesome realtime, mobileview optimized search for your wordpress blog, powered by appbase.io.
+Author: Yash Shah
+Author URI: https://twitter.com/yashshah
+*/
+
+// For debugging purposes
+//error_reporting(E_ALL);
+//ini_set("display_errors", 1); 
+//define('WP-DEBUG', true);
+
+if (!defined('ABSPATH')) {
+	die('script kiddies, f*ck off');
+}
+
+global $pluginUrl,$pluginDir;
+
+$pluginDir=dirname(__FILE__)."/";
+$pluginUrl=WP_CONTENT_URL."/plugins/".  basename($pluginDir)."/";
+
+define('METHI_VERSION', '0.1');
 
 class MethiSearch {
 
     private static $pluginurl = "";
     private static $plugindir = "";
 	private static $notauthenticate = 0;
-	
+
+/* ------------------------------------------------
+	Option with stores the methi secret key to index posts.
+------------------------------------------------ */	
 	private static $METHI_SECRETKEY_ARRAY = "methi-secretkey-array";
 
-
+/* ------------------------------------------------
+	Constructor to initialize the all action of plugin
+------------------------------------------------ */
     public function __construct() {
 
         global $pluginUrl, $pluginDir, $post, $wpdb;
 		
         self::$pluginurl = $pluginUrl;
-
         self::$plugindir = $pluginDir;
 
         add_action("admin_init", array($this, "count_total_post"));
-
         add_action('admin_enqueue_scripts', array($this, 'addscriptAdmin'));
-
         add_action('wp_footer', array($this, 'addsearchscriptfooter'));
-
         add_action("save_post", array($this, "add_wpdata_to_appbase"));
-		
         add_action('admin_menu', array($this, "my_plugin_menu"));
 		
 		add_action( 'wp_ajax_reindex_all_data', array($this, "reindex_all_data"));
 		add_action( 'wp_ajax_nopriv_reindex_all_data', array($this, "reindex_all_data"));
 		
-		//add_action('methiAuth', array($this, "methiAuth"));
 		add_action( 'wp_ajax_methiAuth', array($this, "methiAuth"));
 		add_action( 'wp_ajax_nopriv_methiAuth', array($this, "methiAuth"));
-		
-		//add_action('home_page_import_all_data', array($this, 'home_page_import_all_data'), 8);
 		
 		add_action('wp_head', array($this, "pluginname_ajaxurl"));
     }
 	
+/* ------------------------------------------------
+	Get count of total publish post
+------------------------------------------------ */
 	public function count_total_post()
 	{
 		$count_posts = wp_count_posts()->publish;
 	}
-	
+
+/* ------------------------------------------------
+	Menu option for plugin
+------------------------------------------------ */	
 	function my_plugin_menu() {
-		
         add_menu_page('Methi Search', 'Methi Search', 8, 'methi_search', array($this, 'methiAuth'));
-
         add_submenu_page('methi_search', 'Methi Search', 'Settings', 8, 'settings', array($this, 'reindex_all_data'));
-
     }
-	
+
+/* ------------------------------------------------
+	Get path of default ajax of wordpress admin file
+------------------------------------------------ */		
 	function pluginname_ajaxurl() {
 	?>
 	<script type="text/javascript">
@@ -60,35 +87,38 @@ class MethiSearch {
 	<?php
 	}
 
+/* ------------------------------------------------
+	Methi Authentication of secrent key with index all post.
+------------------------------------------------ */	
 	function methiauth() {
 	
 	$my_simple_links = get_option(self::$METHI_SECRETKEY_ARRAY);
 		
-	if(empty($my_simple_links) || $my_simple_links  == "" || $my_simple_links == NULL)
-	{	
-	
-		if($_POST && isset($_POST["submitallpost"]) && $_POST["submitallpost"] == 1)
-		{
-			$APPBASE_SECRET_KEY = $_REQUEST['appbasesecretkey'];
-			$result = explode (":", $APPBASE_SECRET_KEY);
-			$keyarray = array("app_name", "write_access_username", "write_access_password", "read_access_username", "read_access_password");
-			
-			$finalsecretarray = array_combine($keyarray, $result);
-			
-			if(!empty($finalsecretarray))
+		if(empty($my_simple_links) || $my_simple_links  == "" || $my_simple_links == NULL)
+		{	
+		
+			if($_POST && isset($_POST["submitallpost"]) && $_POST["submitallpost"] == 1)
 			{
-				update_option(self::$METHI_SECRETKEY_ARRAY, $finalsecretarray );
-				$this->indexallpost();
+				$APPBASE_SECRET_KEY = $_REQUEST['appbasesecretkey'];
+				$result = explode (":", $APPBASE_SECRET_KEY);
+				$keyarray = array("app_name", "write_access_username", "write_access_password", "read_access_username", "read_access_password");
+				
+				$finalsecretarray = array_combine($keyarray, $result);
+				
+				if(!empty($finalsecretarray))
+				{
+					update_option(self::$METHI_SECRETKEY_ARRAY, $finalsecretarray );
+					$this->indexallpost();
+				}
 			}
+			include 'methiauthentication.php';
 		}
-        include 'methiauthentication.php';
-	}
-	else
-	{
-		do_action("wp_ajax_reindex_all_data");
-	}
+		else
+		{
+			do_action("wp_ajax_reindex_all_data");
+		}
     }
-	
+		
 	public function redirect($url)
 	{
 		if(!headers_sent())
@@ -97,16 +127,18 @@ class MethiSearch {
 		}
 		else
 		{
-		?>
-		<script type="text/javascript">
-			window.location.href = "<?php echo $url; ?>";
-		</script>
-		<?php
+			?>
+			<script type="text/javascript">
+				window.location.href = "<?php echo $url; ?>";
+			</script>
+			<?php
 		}
 		exit();
 	}
 	
-	
+/* ------------------------------------------------
+	Methi index all publish post
+------------------------------------------------ */		
 	public function indexallpost(){
 	
 	$my_simple_links = get_option(self::$METHI_SECRETKEY_ARRAY);
@@ -181,7 +213,6 @@ class MethiSearch {
         'body' => '{ "article": { "properties": { "title": { "type": "multi_field", "fields": { "title_simple": { "type": "string", "analyzer": "title_default_analyzer" }, "title_ngrams": { "type": "string", "index_analyzer": "nGram_analyzer", "search_analyzer": "whitespace_analyzer" } } }, "meta_description": { "type": "string", "analyzer": "body_analyzer" }, "tags": { "type": "string", "index": "not_analyzed" }, "keywords": { "type": "string", "index": "not_analyzed" }, "link": { "type": "string", "index": "not_analyzed" }, "image_url": { "type": "string", "index": "not_analyzed" }, "videos_url": { "type": "string", "index": "not_analyzed" }, "body": { "type": "string", "analyzer": "body_analyzer" }, "updated_at": { "type": "date", "format" : "yyyy-MM-dd HH:mm:ss" }, "created_at": { "type": "date", "format" : "yyyy-MM-dd HH:mm:ss" } } } }'
       );
 	$setmappingresponse = wp_remote_post("http://".$my_simple_links['write_access_username'].":".$my_simple_links['write_access_password']."@scalr.api.appbase.io/".$my_simple_links['app_name']."/_mapping/article", $setmappingargs);
-	
 	/* Set Mapping end */
 	
 	$postcount = 0; 
@@ -225,7 +256,6 @@ class MethiSearch {
       $postcount++;
 		if ( is_wp_error( $response ) ) {
 		   $error_message = $response->get_error_message();
-		   //echo "Something went wrong: $error_message";
 		   echo "error";
 		} 
 		else {
@@ -244,8 +274,10 @@ class MethiSearch {
 	wp_die();
 	}
 	
-	
-	function reindex_all_data() {
+/* ------------------------------------------------
+	Methi Reindex all post
+------------------------------------------------ */	
+  function reindex_all_data() {
 	
 	$my_simple_links = get_option(self::$METHI_SECRETKEY_ARRAY);
 		
@@ -259,10 +291,12 @@ class MethiSearch {
 		$this->indexallpost();
 		wp_die();
 	}
-   
 	include "settings.php";
   }
-  
+
+/* ------------------------------------------------
+	Methi include the js in lib folder
+------------------------------------------------ */	
     public function addscriptAdmin() {
 	?>
 	<script type="text/javascript">
@@ -271,7 +305,10 @@ class MethiSearch {
 	<?php
         wp_enqueue_script("methi-serach-admin", self::$pluginurl . "lib/js/search.js");
     }
-	
+
+/* ------------------------------------------------
+	Include Methi Search Script For the Front Side Search.
+------------------------------------------------ */		
 	public function addsearchscriptfooter() {
 			$my_simple_links = get_option(self::$METHI_SECRETKEY_ARRAY);
             ?>
@@ -282,14 +319,12 @@ class MethiSearch {
 			wp_enqueue_script("methi-serach-fromt", self::$pluginurl . "lib/js/search-front.js");
     }
 
-
+/* ------------------------------------------------
+	Index post when new post inserted or reindex when existing post updated.
+------------------------------------------------ */
    function add_wpdata_to_appbase($post_id) {
 	
 		$post = wp_get_single_post( $post_id );
-		
-		/* echo "<pre>";
-		print_r($post);
-		die(); */
 
 		if($post->post_status == 'publish')
 		{
@@ -309,10 +344,7 @@ class MethiSearch {
 						array_push($tagarray, $tag->name);
 					}
 					$tagstring = implode(',',$tagarray); 
-				}
-				
-				
-				
+				}	
 				//echo $tagstring; die; 
 				
 				  $args = array(
@@ -346,3 +378,7 @@ class MethiSearch {
   }
 }
 new MethiSearch();
+
+/* ------------------------------------------------
+	Create the object of methi search class.
+------------------------------------------------ */
