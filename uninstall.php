901d7db70436bc31befd91b948a44d8e9d3613c1
Yash Shah <mail@yashshah.com>
2015-09-16 11:55:11 -0700

    Clean uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,12 +1,8 @@
 <?php
-
-// If uninstall is not called from WordPress, exit
-if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-    exit();
-}
- 
-$option_name = 'methi-secretkey-array';
- 
-delete_option( $option_name );
-
+	// If uninstall is not called from WordPress, exit
+	if(!defined('WP_UNINSTALL_PLUGIN')){
+		exit();
+	}
+	$option_name = 'methi-secretkey-array';
+	delete_option( $option_name );
 ?>
