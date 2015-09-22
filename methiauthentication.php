b8530a921d5fb2fd6686b1a53d9d7e9b23d0e168
Yash Shah <mail@yashshah.com>
2015-09-22 14:09:57 -0700

    Remove css from methiauthentication.php

--- methiauthentication.php
+++ methiauthentication.php
@@ -1,6 +1,8 @@
 <?php global $pluginUrl; ?>
+
+<div class="methi-container">
 <h2>Methi Search Plugin</h2>
-<form name="autentication_form" action="" method="post">
+<form name="autentication_form" action="" method="post" class="methi-mainform">
 	<table id="methiauth"> 
 		<tr>
 			<h3>Get your Methi Magic Key</h3>
@@ -16,18 +18,15 @@
 				sync new posts and changes to existing post data.
 			</p>
 		</tr>
-		<tr>
+		<tr class="form-row">
 			<td>Methi Magic Key: </td>
 			<td><input type='text' name='appbase_secret_token' class="appbaseform-elements" id="appbase_secret_token" />
-			</td>
-		</tr>
-		<tr>
-			<td colspan="3">
-				<input type="button" name="submitallpost" value="Sync Data" id="submitallpost" class="indexpostbutton"/>
+			<input type="button" name="submitallpost" value="Sync Data" id="submitallpost" class="indexpostbutton"/>
 			</td>
 		</tr>
 	</table>
 </form>
+</div>
 <?php $count_posts = wp_count_posts()->publish; ?>
 <div id="import_record_process">
 	<h2>Syncing all posts...</h2>
@@ -42,54 +41,7 @@
 		<div class="totalpostcount">
 			<span id="postcountprogress">0</span>/
 			<?php echo $count_posts; ?>
-			<div>
-			</div>
 		</div>
 	</div>
 </div>
 
-<style type="text/css">
-	.appbaseform-elements {
-		width:400px;
-	}
-	#import_record_process {
-		background :#DDD;
-		padding:5px 15px 20px;
-		width:400px;
-		margin:20px 0;
-		display:none;
-	}
-	.percentagebar {
-		display:inline-block;
-		width:100%;
-		height:15px;
-		background:#ccc;
-		position:relative;
-		margin:5px 0 20px 0;
-	}
-	.actualprocess {
-		display:inline-block;
-		width:0%;
-		height:15px;
-		background:#019183;
-		transition: all 0.5s ease 0s;
-		-webkit-transition: all 0.5s ease 0s;
-		-o-transition: all 0.5s ease 0s;
-		-ms-transition: all 0.5s ease 0s;
-		-moz-transition: all 0.5s ease 0s;
-	}
-	.progress_container {
-		display:block;
-		width:100%;
-		font-size:17px;
-	}
-	.percentage_container {
-		font-size:17px;
-		font-weight:bold;
-		display:inline;
-	}
-	.totalpostcount {text-align :right;
-		float:right;
-		display:block;
-	}
-</style>
